713951861ee478703b616b2d8117d5def6b5557b
Chip Jarred <chipjarred@gmail.com>
2021-03-11 02:08:31 -0600

    Added IPv6 and Unix domain client-server tests.

--- Tests/JSONRPCTests/Client_Server_UnitTests.swift
+++ Tests/JSONRPCTests/Client_Server_UnitTests.swift
@@ -74,4 +74,129 @@ class Client_Server_UnitTests: XCTestCase
         
         sem.wait(); sem.signal()
     }
+    
+    // -------------------------------------
+    func test_client_can_connect_to_inet6_server()
+    {
+        let serverAddress = SocketAddress(
+            ip6Address: .loopback,
+            port: 2000 + #line
+        )
+        
+        // -------------------------------------
+        final class ServerSessionDelegate: JSONRPCSessionDelegate
+        {
+            func respond(
+                to request: Request,
+                for _ :JSONRPCSession) -> Response?
+            {
+                if request.method == "foo" {
+                    return response(for: request, result: "bar")
+                }
+                return nil
+            }
+        }
+        
+        guard let server = JSONRPCServer(
+            boundTo: serverAddress,
+            maximumConnections: 1,
+            typeOfDelegate: ServerSessionDelegate.self)
+        else
+        {
+            XCTFail("Unable to create server")
+            return
+        }
+        defer { server.terminate(.immediately) }
+        
+        server.start()
+        
+        guard let client = JSONRPCSession(serverAddress: serverAddress) else
+        {
+            XCTFail("Unable to create client-side session")
+            return
+        }
+        
+        let sem = DispatchSemaphore(value: 0)
+        
+        client.request(method: "foo")
+        {
+            switch $0
+            {
+                case.success(let result):
+                    switch result
+                    {
+                        case .string(let s): XCTAssertEqual(s, "bar")
+                        default: XCTFail("Result is wrong type: \(result)")
+                    }
+                case .failure(let error):
+                    XCTFail("Got Error response: \(error)")
+
+            }
+            sem.signal()
+        }
+        
+        sem.wait(); sem.signal()
+    }
+    
+    // -------------------------------------
+    func test_client_can_connect_to_Unix_domain_server()
+    {
+        let serverAddress = SocketAddress(
+            unixPath: UnixSocketPath("\(#function)-server")!
+        )
+        
+        // -------------------------------------
+        final class ServerSessionDelegate: JSONRPCSessionDelegate
+        {
+            func respond(
+                to request: Request,
+                for _ :JSONRPCSession) -> Response?
+            {
+                if request.method == "foo" {
+                    return response(for: request, result: "bar")
+                }
+                return nil
+            }
+        }
+        
+        guard let server = JSONRPCServer(
+            boundTo: serverAddress,
+            maximumConnections: 1,
+            typeOfDelegate: ServerSessionDelegate.self)
+        else
+        {
+            XCTFail("Unable to create server")
+            return
+        }
+        defer { server.terminate(.immediately) }
+        
+        server.start()
+        
+        guard let client = JSONRPCSession(serverAddress: serverAddress) else
+        {
+            XCTFail("Unable to create client-side session")
+            return
+        }
+        
+        let sem = DispatchSemaphore(value: 0)
+        
+        client.request(method: "foo")
+        {
+            switch $0
+            {
+                case.success(let result):
+                    switch result
+                    {
+                        case .string(let s): XCTAssertEqual(s, "bar")
+                        default: XCTFail("Result is wrong type: \(result)")
+                    }
+                case .failure(let error):
+                    XCTFail("Got Error response: \(error)")
+
+            }
+            sem.signal()
+        }
+        
+        sem.wait(); sem.signal()
+    }
 }
